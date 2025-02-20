7872be879a3bf4bcf3abe0d318a17d9cc58145ce
Otito Amuga <otitoamuga@gmail.com>
2025-02-19 21:57:22 -0500

    ------___..____------

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,7 +1,7 @@
 function MainContent() {
     return (
         <main>
-            <h2>We are animal shelter and we seek to find a place for animals to live, for a small charge</h2>
+            <h2>We are animal shelter and we seek to find a place for animals to live. Tell us your story</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur eaque error ipsam minus molestiae necessitatibus officia ratione, sapiente velit vitae!</p>
         </main>
     );
